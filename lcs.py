fd2404c08c9c856132ce634b4dc42c741fa5914e
LightRomance <83913037+LightRomance@users.noreply.github.com>
2023-10-29 20:07:49 -0400

    Update lcs.py

--- lcs.py
+++ lcs.py
@@ -6,7 +6,7 @@ def generate_random_string(length):
     # Generates a string containing random characters
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
 
-def longestCommonSubsequence(str1, str2) -> int:
+def longestCommonSubsequence(str1, str2) -> (int, str):
     n1, n2 = len(str1), len(str2)
     dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
     for i in range(1, n1 + 1):
@@ -15,7 +15,20 @@ def longestCommonSubsequence(str1, str2) -> int:
                 dp[i][j] = 1 + dp[i-1][j-1]  # Increase the LCS length by 1 when characters match
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])  # Take the maximum of the two adjacent cells
-    return dp[-1][-1]
+    # Reconstruct the LCS string
+    i, j = n1, n2
+    lcs = []
+    while i > 0 and j > 0:
+        if str1[i - 1] == str2[j - 1]:
+            lcs.insert(0, str1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return dp[-1][-1], ''.join(lcs)
 
 # Test time complexity
 input_sizes = [10, 50, 100, 500, 1000, 10000]  # Different lengths of strings
@@ -34,6 +47,7 @@ for size in input_sizes:
     execution_time = end_time - start_time
     print(f"Input size: {size}, Execution time: {execution_time:.6f} seconds")
     
-    # Show LCS length to verify
-    if size <= 20:
-        print(f"Longest Common Subsequence Length: {result}\n")
+    # Show LCS length and String to verify
+    if size <= 100:
+        print(f"Longest Common Subsequence Length: {result_length}")
+        print(f"Longest Common Subsequence: {result_string}\n")
